b0e33b1b1ca38d5245e0933cf7492a533bdc2cd5
bipeti <peter.bihari@gmail.com>
2023-08-10 05:35:35 +0200

    Google Analytics

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -6,9 +6,9 @@ export default function Welcome() {
                 Bihari Péter vagyok, frontend fejlesztő. <br />
                 <br />
                 Gyerekkorom óta foglalkozom programozással és az évek során
-                számos kkülönféle programozási nyelveket tanultam. A Delphi-t és
-                az SQL-t még tantermi keretek között ismertem meg, később a
-                PHP-t és a MySQL-t önképzéssel sajátítottam el. <br />
+                számos különféle programozási nyelvet tanultam. A Delphi-t és az
+                SQL-t még tantermi keretek között ismertem meg, később a PHP-t
+                és a MySQL-t önképzéssel sajátítottam el. <br />
                 Jelenleg a Javascript, Typescript és React nyelvekkel dolgozom
                 elsősorban. Ezekben a nyelvekben mozgok igazán otthonosan és az
                 oldalon elérhető projektjeim is ezekre a technológiákra épülnek.
